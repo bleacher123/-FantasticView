45e84d03cd85efdc0fb58ec5ac1219d189eb32a7
bleacher123 <teriyakibob@FludddeMacBook-Pro.local>
2017-08-05 23:51:02 +0800

    init

---
+++ FantasticView/FantasticView.swift
@@ -0,0 +1,33 @@
+//
+//  FantasticView.swift
+//  FantasticView
+//
+//  Created by teriyakibob on 2017/8/5.
+//  Copyright © 2017年 hoodsound. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: UIView {
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    var colorCounter = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+            UIView.animate(withDuration: 2.0) {  //2
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                self.colorCounter += 1  //4
+            }
+        }
+        
+        scheduledColorChanged.fire()  //5
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
